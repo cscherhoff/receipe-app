945b0999600e1dd074c2eb2221f8e7b000f1b5d5
cscherho <cscherhoff@web.de>
2022-06-05 11:05:37 +0200

    Add content to recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'https://theunplannedplanner.com/wp-content/uploads/2020/12/recipe-575434_1280-1140x973.png')
+  ];
 
   constructor() { }
 
